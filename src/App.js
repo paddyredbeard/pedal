e4c82ffacda1b1db6f8ba19eed0f0cbf7055c987
Patrick Barabe <paddyredbeard@gmail.com>
2021-07-24 13:45:54 -0700

    First draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import instaIcon from './instagram.png'
+import ytIcon from './youtube.png'
+import Playlist from './Playlist'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='app-header'>
+        <img src={logo} className='app-logo' alt='logo' />
+        <h1>
+          Eat Tacos, Be Kind, and Ride Your Bike!
+        </h1>
       </header>
+
+      <header className='sub-header'>
+        <h2>Not Necessarily In That Order</h2>
+      </header>
+
+      <Playlist />
+
+      <footer className='footer'>
+        <div className='signature'>
+          <div>pddyrdbrd</div>
+          <div>17 jul 2021</div>
+          <div>14zgu3z0qe3az4a</div>
+        </div>
+
+        <div className='links'>
+          <a href='https://instagram.com/paddyredbeard' title='@paddyredbeard'>
+            <img src={instaIcon} alt='Instagram icon' />
+          </a>
+
+          <a href='https://www.youtube.com/c/PatrickBarabe' title='Patrick on YouTube'>
+            <img src={ytIcon} alt='YouTube icon' />
+          </a>
+        </div>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders page', () => {
+  render(<App />)
+  const h1Element = screen.getByText(/eat tacos, be kind, and ride your bike/i)
+  const h2Element = screen.getByText(/not necessarily in that order/i)
+  expect(h1Element).toBeInTheDocument()
+  expect(h2Element).toBeInTheDocument()
+})
